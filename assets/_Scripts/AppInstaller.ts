d0cf61854466ee55859a0dfb8c6d12d7025bec4f
Mikhail A <mixadze1@bk.ru>
2025-05-08 19:05:04 +0300

    [add] models + [add] folders scripts

---
+++ assets/_Scripts/AppInstaller.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('AppInstaller')
+export class AppInstaller extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
