7d2a0aab20b614d4741828e57ce697c9f55fedcd
Mikhail A <mixadze1@bk.ru>
2025-05-08 19:14:52 +0300

    [add] create game scene + [add] widght height 1080 2560

--- assets/_Scripts/AppInstaller.ts
+++ assets/_Scripts/AppInstaller.ts
@@ -2,13 +2,9 @@ import { _decorator, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('AppInstaller')
-export class AppInstaller extends Component {
-    start() {
-
-    }
-
-    update(deltaTime: number) {
-        
-    }
+export class AppInstaller  
+{
+   
 }
-
+
+
